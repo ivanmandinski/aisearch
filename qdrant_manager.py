20b50baac0f3d9c2e60bf0a9803063d892e38585
ivanmandinski <ivanmandinski@tessa.tech>
2025-10-08 12:07:39 +0300

    Add files via upload

--- qdrant_manager.py
+++ qdrant_manager.py
@@ -6,7 +6,7 @@ from typing import List, Dict, Any, Optional, Tuple
 from qdrant_client import QdrantClient
 from qdrant_client.models import (
     Distance, VectorParams, PointStruct, Filter, FieldCondition, 
-    MatchValue, SearchRequest, Query
+    MatchValue, SearchRequest
 )
 from qdrant_client.http import models
 import numpy as np
